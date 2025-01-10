deb8088a5fcd14271f7071423200fd7af96adb5c
Kaevrin <159499822+Kaevrin@users.noreply.github.com>
2025-01-10 15:10:13 -0700

    Completed W01 projects

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Please enter your first name:");
+        string firstname = Console.ReadLine();
+        Console.Write("Please enter your last name:");
+        string lastname = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your score?");
+        string input = Console.ReadLine();
+        int score = int.Parse(input);
+        string gradeResult = "";
+        if (score >= 90)
+        {
+            gradeResult = "A";
+        }
+        else if (score >= 80)
+        {
+            gradeResult = "B";;
+        }
+        else if (score >= 70)
+        {
+            gradeResult = "C";;
+        }
+        else if (score >= 60)
+        {
+            gradeResult = "D";;
+        }
+        else
+        {
+            gradeResult = "F";
+        }
+            Console.WriteLine($"Your grade is {gradeResult}");
+        if (score >= 70)
+        {
+            Console.Write("You are on track!");
+        }
+        
+        else
+        {
+            Console.Write("Keep up the hard work to bring it up!");
+        }
     }
 }
