a419f362e803bec2e9b23fb5716dc8fa76b64bc8
Kaevrin <159499822+Kaevrin@users.noreply.github.com>
2025-01-22 19:45:30 -0700

    Resume program completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "FrostGiant";
+        job1._jobTitle = "Rock Climber";
+        job1._startYear=1997;
+        job1._endYear=2006;
+        Job job2 = new Job();
+        job2._company = "FreeJam";
+        job2._jobTitle = "Design Jammer";
+        job2._startYear =2007;
+        job2._endYear = 2017;
+        Resume resume1 = new Resume();
+        resume1._name = "John Johnson";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
+        
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        string adjective = GetAdjective();
+        string noun = GetNoun();
+
+        int number = Multiply(3,4);
+
+        Console.WriteLine($"I looked out the window and saw {number} {adjective} {noun}");
     }
-}
+
+    static int Multiply(int number1, int number2)
+    {
+        int product = number1 * number2;
+        return product;
+    }
+
+    static string GetAdjective()
+    {
+        string adjective = "yellow";
+        return adjective;
+    }
+    static string GetNoun()
+    {
+        string noun = "bird";
+        return noun;
+    }
+}  
