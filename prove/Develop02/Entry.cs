1a7cb038c90fa5bea2b22ac3b375d887fe8e2199
Kaevrin <159499822+Kaevrin@users.noreply.github.com>
2025-01-28 15:39:07 -0700

    Beginning Journal project

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,9 @@
+public class Entry
+{
+    public string _entry = "";
+    public void Input()
+    {
+        _entry = Console.ReadLine();
+        Console.WriteLine($"{_entry}");
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        Entry EntryInstance = new Entry();
         Console.WriteLine("Hello Develop02 World!");
+        EntryInstance.Input();
     }
 }
