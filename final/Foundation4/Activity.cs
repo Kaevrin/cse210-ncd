a4efc037849e7107aa29c9a9cdc1733d185be540
Kaevrin <159499822+Kaevrin@users.noreply.github.com>
2025-04-03 15:56:33 -0600

    Reworking for inherited math.

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -1,3 +1,5 @@
+using System.Xml;
+
 class Activity {
     protected DateTime _date;
     protected int _minutes;
@@ -9,7 +11,19 @@ class Activity {
     }
 
 
-    protected virtual string GetSumary() {
+    public virtual string GetSummary() {
         return "Error, no override given.";
+    }
+    protected virtual double GetDistance(double speed, double time) {
+        double output = speed * (time/60);
+        return output;
+    }
+    protected virtual double GetSpeed(double pace) {
+        double output = _minutes / pace;
+        return output;
+    }
+    protected virtual double GetPace(double distance) {
+        double output = _minutes / distance;
+        return output;
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation4 World!");
+        Tempcode tempcode = new Tempcode(new DateTime(2025, 03, 09), 50, 30);
+        Console.WriteLine(tempcode.GetSummary());
     }
 }
